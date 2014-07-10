fc72f5ec602a64176860f7c79813197d976248f3
jadedkitty <jadedkittycat@gmail.com>
2014-07-10 10:16:35 -0700

    Added kitty block, started adding creative tab

---
+++ src/main/java/com/jadedkitty/kittyStuff2/block/BlockKittyStuff.java
@@ -0,0 +1,7 @@
+package com.jadedkitty.kittyStuff2.block;
+
+/**
+ * Created by jadedkitty on 7/10/2014.
+ */
+public class BlockKittyStuff {
+}
---
+++ src/main/java/com/jadedkitty/kittyStuff2/init/modblocks.java
@@ -0,0 +1,7 @@
+package com.jadedkitty.kittyStuff2.init;
+
+/**
+ * Created by jadedkitty on 7/10/2014.
+ */
+public class modblocks {
+}
