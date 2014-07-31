3d8521157c83779a476d07379daa2490c9350df3
jadedkitty <jadedkittycat@gmail.com>
2014-07-31 11:49:02 -0700

    Added pasteurizer recipe
    Added a recipe for the pasteurizer

--- src/main/java/com/jadedkitty/kittyStuff2/init/recipes.java
+++ src/main/java/com/jadedkitty/kittyStuff2/init/recipes.java
@@ -10,11 +10,11 @@ public class recipes
    public static void init()
    {
        GameRegistry.addRecipe(new ItemStack(modItems.kittyClaws), " c ", "ckc", " s ", 'c', new ItemStack(modItems.kittyClaw),'k',new ItemStack(modItems.kittyGem),'s',new ItemStack(Items.diamond_sword));
-       GameRegistry.addShapelessRecipe(new ItemStack(modItems.kittyClaw), new ItemStack(modItems.kittyGem));
        GameRegistry.addRecipe(new ItemStack(modblocks.kittyblock), "kkk", "kkk", "kkk", 'k',new ItemStack(modItems.kittyGem));
        GameRegistry.addShapelessRecipe(new ItemStack(modItems.kittyGem), new ItemStack(Items.diamond), new ItemStack(Items.dye, 1, 5));
        GameRegistry.addRecipe(new ItemStack(modblocks.kittyblock), "www", "wbw", "www",'b',new ItemStack(Blocks.diamond_block), 'w',new ItemStack(Blocks.wool, 1, 9));
        GameRegistry.addSmelting(modItems.pasteurizedMilk, new ItemStack(modItems.warmMilk), 0);
        PasteurizerRecipes.smelting().addSmelting(new ItemStack(Items.milk_bucket,1),new ItemStack(modItems.pasteurizedMilk,1),1.0F);
+       GameRegistry.addRecipe(new ItemStack(modblocks.pasteurizerIdle), "b b", "b b", "bkb", 'k',new ItemStack(modItems.kittyGem), 'b',new ItemStack(Items.bucket));
    }
 }
